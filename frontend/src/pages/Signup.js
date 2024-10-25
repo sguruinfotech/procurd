aa029996f67434e7ef8a4e69fff15be16442f793
sguruinfotech <sales@amarucity.com>
2024-10-25 14:51:27 +0530

    test12

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -40,8 +40,7 @@ export default function Signup() {
                 <div className='regquote'>Already have an account?  <span><Link to="/Login">Login</Link></span></div>
                
         </div>
-       <p>update from vs</p>
-       <p>update from git</p>
+       
        
     </div>
     </>
